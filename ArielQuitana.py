7fada550e0f20e82e6d2e6030ef5786eea9b666e
Cool-Ari-Programmer <arie.quintana@duocuc.cl>
2025-07-14 19:43:24 -0400

    Error handler (Aka type verification) added

---
+++ ArielQuitana.py
@@ -0,0 +1,55 @@
+# Variables
+
+
+Products = {
+    "Modelname": ['brand next to it display measurements:', 1.2, "RAM", "DDisk Type","Capacity","PRocessor","Graphics card"]
+}
+
+Stock= {
+    "Modelname": ["Cost", "Stock"]
+}
+
+user_choice: 0
+
+
+# Variables/
+
+
+
+
+
+
+print("******Menu principal******")
+print("1. Stock disponible")
+print("2. Busqueda por precio")
+print("3. Actualizar precio")
+print("4. Salir")
+
+
+while True:
+
+    while True:
+
+        try:
+            user_choice  = int(input())
+        except ValueError:
+            print("Texto detectado") 
+        else: 
+            break
+        
+    if user_choice == 4:
+        print("Goobie")
+        break
+
+
+
+        
+
+
+
+
+
+
+
+
+
